2ef7a4d28f59de3834a435f84eaa42ad682112b1
aryan5678 <ayusharyanjha946@gmail.com>
2026-04-01 19:49:15 +0530

    add new folder

--- Call Backs/script.js
+++ Call Backs/script.js
@@ -18,22 +18,41 @@
 // fetchData(handleData);
 
 
-function fetchdData(callback){
+// function fetchdData(callback){
+//     setTimeout(()=>{
+//         let data = 'fetched data';
+//         callback(data, null);
+//     }, 5000)
+// }
+
+// function handleData(data, error){
+//     if(error){
+//         console.error(error);
+//     }else{
+//         console.log(data)
+//     }
+
+    
+
+// }
+
+// console.log('start')
+
+// fetchdData(handleData)
+
+function fetchData(callback){
     setTimeout(()=>{
-        let data = 'fetched data';
-        callback(data, null);
-    }, 5000)
+        let data = 'fetch data';
+        callback(data, 'server error');
+    }, 3000)
 }
 
 function handleData(data, error){
     if(error){
-        console.error(error);
+        console.error('Server error');
     }else{
-        console.log(data)
+        console.log(data);
     }
-
-    
-
 }
-
-fetchdData(handleData)
+console.log('Start');
+fetchData(handleData);
