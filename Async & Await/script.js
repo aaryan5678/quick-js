0999edd9992eba853c69da6444a2a716c2eef15c
aryan5678 <ayusharyanjha946@gmail.com>
2026-04-04 16:26:25 +0530

    add new folder

--- Async & Await/script.js
+++ Async & Await/script.js
@@ -1,8 +1,21 @@
-async function yaahudi(params) {
-    try{
+function getdata(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve('data fetched');
+            reject('server error');
+        }, 3000);
+    })
+}
+
+async function handleData(params) {
+    try {
+        const result = await getdata();
+        console.log(result);
+    }
+    catch(error){
+console.error(error);
 
     }
-    catch{
-        
-    }
 }
+
+handleData();
